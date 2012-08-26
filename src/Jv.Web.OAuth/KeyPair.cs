19e9c19790ea7571b34e06a503cc759b4a679cad
João Vitor P. Moraes <jvlppm@gmail.com>
2012-08-26 10:20:53 -0300

    INT: Classe KeyPair para guardar tokens.

---
+++ src/Jv.Web.OAuth/KeyPair.cs
@@ -0,0 +1,40 @@
+﻿using Jv.Json;
+using System;
+
+namespace Jv.Web.OAuth
+{
+    public class KeyPair
+    {
+        #region Properties
+        public string Key { get; private set; }
+        public string Secret { get; private set; }
+        #endregion
+
+        #region Constructors
+        public KeyPair(string key, string secret)
+        {
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentNullException("key");
+
+            if (string.IsNullOrEmpty(secret))
+                throw new ArgumentNullException("secret");
+
+            Key = key;
+            Secret = secret;
+        }
+        #endregion
+
+        #region Serialization
+        public override string ToString()
+        {
+            return new { key = Key, secret = Secret }.ToJson();
+        }
+
+        public static KeyPair Parse(string keyPair)
+        {
+            var pair = keyPair.AsJson();
+            return new KeyPair(pair.key, pair.secret);
+        }
+        #endregion
+    }
+}
