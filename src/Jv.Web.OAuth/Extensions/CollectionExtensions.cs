02738b860718518b6e6a700ef5658cd2776408d8
João Vitor P. Moraes <jvlppm@gmail.com>
2012-09-01 22:38:50 -0300

    INT: Classe HttpParameters.

---
+++ src/Jv.Web.OAuth/Extensions/CollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jv.Web.OAuth.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static void Add<TKey, TValue>(this ICollection<KeyValuePair<TKey, TValue>> collection, TKey key, TValue value)
+        {
+            collection.Add(new KeyValuePair<TKey,TValue>(key, value));
+        }
+
+        public static HttpParameters Union(this HttpParameters first, IEnumerable<KeyValuePair<string, string>> second)
+        {
+            var result = new HttpParameters(first);
+            result.AddRange(second);
+            return result;
+        }
+    }
+}
---
+++ src/Jv.Web.OAuth/HttpParameters.cs
@@ -0,0 +1,59 @@
+﻿using Jv.Web.OAuth.Extensions;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.Storage;
+
+namespace Jv.Web.OAuth
+{
+    public class HttpParameters : IEnumerable<KeyValuePair<string, object>>, IEnumerable<KeyValuePair<string, string>>, IEnumerable<KeyValuePair<string, StorageFile>>
+    {
+        List<KeyValuePair<string, object>> _parameters = new List<KeyValuePair<string, object>>();
+
+        public HttpParameters(HttpParameters parameters = null)
+        {
+            if (parameters != null)
+                _parameters.AddRange(parameters._parameters);
+        }
+
+        public void Add(string name, string value)
+        {
+            _parameters.Add(name, value);
+        }
+
+        public void Add(string name, StorageFile value)
+        {
+            _parameters.Add(name, value);
+        }
+
+        public void AddRange(IEnumerable<KeyValuePair<string, string>> keyValues)
+        {
+            _parameters.AddRange(keyValues.Select(kv => new KeyValuePair<string, object>(kv.Key, kv.Value)));
+        }
+
+        public void AddRange(IEnumerable<KeyValuePair<string, StorageFile>> keyValues)
+        {
+            _parameters.AddRange(keyValues.Select(kv => new KeyValuePair<string, object>(kv.Key, kv.Value)));
+        }
+
+        public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
+        {
+            return _parameters.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _parameters.GetEnumerator();
+        }
+
+        IEnumerator<KeyValuePair<string, string>> IEnumerable<KeyValuePair<string, string>>.GetEnumerator()
+        {
+            return _parameters.OfType<KeyValuePair<string, string>>().GetEnumerator();
+        }
+
+        IEnumerator<KeyValuePair<string, StorageFile>> IEnumerable<KeyValuePair<string, StorageFile>>.GetEnumerator()
+        {
+            return _parameters.OfType<KeyValuePair<string, StorageFile>>().GetEnumerator();
+        }
+    }
+}
