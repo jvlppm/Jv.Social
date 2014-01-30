8289c335b6d73193f6c555d50ef4e690841182c6
Joao Vitor P. Moraes <joao.moraes@lactec.org.br>
2014-01-30 12:52:44 -0200

    OAuthClientBearer.

---
+++ src/Jv.Web.OAuth/v2/OAuthClientBearer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Jv.Web.OAuth.v2
+{
+    public class OAuthClientBearer : OAuthClientBase
+    {
+        public string AccessToken { get; private set; }
+
+        public OAuthClientBearer(KeyPair applicationInfo, string accessToken, HttpClient httpClient = null)
+            : base(applicationInfo, httpClient)
+        {
+            if (accessToken == null)
+                throw new ArgumentNullException("accessToken");
+
+            AccessToken = accessToken;
+        }
+
+        protected override HttpParameters Sign(Uri url, HttpMethod method, HttpParameters parameters)
+        {
+            return new HttpParameters(parameters)
+            {
+                {"access_token", AccessToken}
+            };
+        }
+    }
+}
